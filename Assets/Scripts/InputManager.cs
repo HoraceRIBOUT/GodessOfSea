ab6732df30c809de593fa7d11361c22ed7ced9f1
Aurélien <aurelien.tauvy@etu.unilim.fr>
2019-09-25 17:26:22 +0200

    Wiimote support
    Not tested in real conditions, input prints in console

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+	public static int whichRemote;
+    // Start is called before the first frame update
+    void Start()
+    {
+			Wii.StartSearch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Vector3 input = Wii.GetWiimoteAcceleration(whichRemote);
+        //Debug.Log(input);
+          //Wii.ToggleRumble(whichRemote);
+          if (input.z > 3.0f || input.z < -3.0f){//Decided arbitrarily through tests
+            Debug.Log("Input?");
+          }
+    }
+}
